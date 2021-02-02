e8c1f011eec26fbe454614c5c5bd6452742a8071
Santiago Betancur <scb9908@gmail.com>
2021-02-02 10:38:17 -0500

    Tw implementation

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const twit = require('twit');
+require('dotenv').config({ path: './process.template.env' });
+
+// @packages
+const Twit = require('twit');
+
+// @scripts
 const quotes = require('./quotes');
-const delayTimeInMins = 5; 
-const MILIS_TO_MINS = 3600;
+
+// @constants
+const delayTimeInMins = 10; 
+const MILIS_TO_MINS = 60 * 1000;
+
+const twitterClient = new Twit({
+    consumer_key: process.env.API_CONSUMER_KEY, 
+    access_token: process.env.API_ACCESS_TOKEN,
+    access_token_secret: process.env.API_TOKEN_SECRET,
+    consumer_secret: process.env.API_CONSUMER_SECRET,
+    strictSSL: true,
+    timeout_ms: 30000 
+})
 
 const main = () => {
     const maxNumAvaliable = quotes.length - 1;
     const random = Math.round(Math.random() * maxNumAvaliable);
     console.log(quotes[random]);
+
+    twitterClient.post('statuses/update', { status: quotes[random] }, Function.prototype);
 };
 
-main();
 setInterval(main, delayTimeInMins * MILIS_TO_MINS); 
